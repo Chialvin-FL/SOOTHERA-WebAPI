f997cc1b994185b73f9b2015d196d3d20aa9b49f
ajiwnl <alvinjohnaranetz@gmail.com>
2026-01-13 19:02:24 +0800

    push

---
+++ FL.Basecode.DTOs/Authentication/Register/StatusResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FL.Basecode.DTOs.Authentication
+{
+    public class StatusResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+        public object? Data { get; set; }
+    }
+
+}
